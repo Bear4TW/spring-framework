2fd20aa2b3c6243dffcc01388c48d53462e88715
Bear4TW <darien.ruggles@gmail.com>
2022-02-18 18:11:17 -0600

    Updated wiring and DI

--- Spring-02-Core-Wiring/src/main/java/com/bootcamp/Person.java
+++ Spring-02-Core-Wiring/src/main/java/com/bootcamp/Person.java
@@ -1,5 +1,12 @@
 package com.bootcamp;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
 public class Person {
     private String name;
     private Car car;
